7dfccc9ce99fc35b7a0d049882c7a9fa56719e87
Kushagra-23 <kshgrkumar@gmail.com>
2023-05-09 23:25:31 +0530

    Episode - 01

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement("h1", {
+  id: 'heading',
+  xyz: 'XYZ'
+}, "Hello World from React");
+// console.log(heading)
+
+const parent = React.createElement("div", 
+{
+  id: 'parent'
+}, [React.createElement("h1",{id: 'child'},"H1 TAG"), React.createElement("h2",{},"H2 TAG")]);
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
